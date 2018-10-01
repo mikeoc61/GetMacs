dd9863d54b90380c3456f90ebf5d31ba68b61a61
Michael OConnor <gmikeoc@gmail.com>
2018-10-01 17:11:54 -0500

    Additional output formatting

--- GetMacs.py
+++ GetMacs.py
@@ -1,6 +1,9 @@
 ###########
 # Python3 program to scan the local area network for valid MAC addresses
 #
+# Basically does the same thing that "arp -a" but more brute force and
+# with output formatting.
+#
 # Author - Michael O'Connor
 #
 # Date: 9/10/2017
@@ -18,8 +21,13 @@ def get_macs(net):
 
   host = 1
 
+  # Print out a nice header
+
+  print('      Name                 IP                MAC Addr')
+  print('-' * 60)
+
   while host <= 254:
-     ip_addr = "{}.{}".format(net, str(host))			# Form complete IPv4 address (Python3)
+     ip_addr = "{}.{}".format(net, str(host))		# Form complete IPv4 address (Python3)
      cmd = ["arp", "-i", "en0", ip_addr]			# Form array to be passed to subprocess.Popen()
 
      p_stdout = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=None) # execute command
@@ -28,21 +36,23 @@ def get_macs(net):
 
      out_array = out_str.split()					   # Split string into array of words
 
-     if out_array[0].startswith('?'):					   # Test for valid IP address
-       print ("IP:{}\tMAC:{}".format(out_array[1], out_array[3]))
+     if out_array[3].startswith('no') == False:		   # Test for valid IP address
+        print ("\n{0:20} {1:20} {2:20}".format(out_array[0], out_array[1], out_array[3]))
+#     else:
+#        print('.', end='', flush=True)
 
      host += 1
 
 # Define a main() function that prints a little greeting.
 def main():
-  # Get the cmd from the command line, using 'date' as a fallback.
+  # Get the cmd from the command line, using 'typical /24 LAN' as a fallback.
   if len(sys.argv) >= 2:
     addr = sys.argv[1]
   else:
-    addr = '192.168.0'
+    addr = '192.168.1'
 
   print("Scanning Network: {}".format(addr))
-  
+
   print(get_macs(addr))
 
 # This is the standard boilerplate that calls the main() function.
